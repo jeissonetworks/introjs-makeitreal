e384c15df56c7ef4aeb4ea2ffc900ecce4e9c865
jeissonetworks <>
2021-07-14 21:18:58 +0000

    Ejercicio 6

--- Ejercicio 6/index.js
+++ Ejercicio 6/index.js
@@ -3,10 +3,9 @@
 Si es mayor debe imprimir "El número es mayor a 10".
 
 Si es menor debe imprimir "El número es menor o igual a 10". */
-
 const num = parseInt(prompt('Ingresa un numero'));
 if (num > 10) {
   console.log('El numero es mayor a 10');
 } else {
     console.log('El numero es menor o igual a 10')
-  }
+}
