51968c3881a7387a200ebaf040849adac3eca018
jeissonetworks <>
2021-07-15 18:03:59 +0000

    Ejercicio 12

---
+++ Ejercicio 12/index.js
@@ -0,0 +1,8 @@
+/*Escribe un programa que le pida al usuario ingresar una frase. El programa debe imprimir la frase en la consola 10 veces.
+
+Nota: utiliza un ciclo para imprimir la frase las 10 veces. */
+let frase = prompt('Ingresa una frase')
+
+for (let index = 0; index < 10; index++){
+  console.log(frase)
+}
