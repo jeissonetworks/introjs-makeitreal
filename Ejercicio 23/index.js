eaa3383f0f412288ef0af589b58d9a956888ab8a
jeissonetworks <>
2021-08-11 23:08:30 +0000

    Ejercicio 23

---
+++ Ejercicio 23/index.js
@@ -0,0 +1,10 @@
+/*Escribe un programa que le pida una frase al usuario y la repita en mayúsculas. Por ejemplo:
+
+Ingresa una frase> Hola Mundo
+HOLA MUNDO
+
+*/
+
+let name = (prompt('Ingresa un frase'))
+
+console.log(name.toUpperCase())
