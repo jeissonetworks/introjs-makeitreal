1ec0643a69525a5310ad527a014409f8b776db94
jeissonetworks <>
2021-08-19 22:44:36 +0000

    Ejercicio 50

---
+++ Ejercicio 50/index.js
@@ -0,0 +1,17 @@
+/*
+Crea una función llamada max (puede ser normal o función flecha) que reciba un arreglo de números como argumento y retorne el número mayor.
+
+Nota: No utilices el método Math.max de JavaScript.
+
+// escribe tu solución acá
+
+// código de prueba
+console.log(max([1, 2, 3, 4])) // 4
+console.log(max([63, 85, 39, 24, 3])) // 85
+ */
+
+const max = arr => arr.reduce((a, b) => a > b ? a : b);
+
+
+console.log(max([1, 2, 3, 4]))
+console.log(max([63, 85, 39, 24, 3]))
