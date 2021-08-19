fea6791f4ccbd11d8cdb5019615de85f40d732f1
jeissonetworks <>
2021-08-19 22:14:59 +0000

    Ejercicio 41

---
+++ Ejercicio 41/index.js
@@ -0,0 +1,21 @@
+/*Imprime todos los elementos del arreglo utilizando el método forEach:
+
+const arr = [45, "Hola", 82, 2, 67, true, "Juan", 12.34]
+
+// escribe tu código acá
+El resultado en consola debería ser el siguiente:
+
+45
+Hola
+82
+2
+67
+true
+Juan
+12.34 */
+
+const arr = [45, "Hola", 82, 2, 67, true, "Juan", 12.34]
+
+arr.forEach(ar => {
+  console.log(ar);
+});
