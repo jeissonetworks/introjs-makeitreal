bf1d27a326e584e6a251bac5d7150ba45af293de
jeissonetworks <>
2021-08-19 22:11:47 +0000

    Ejercicio 40

---
+++ Ejercicio 40/index.js
@@ -0,0 +1,21 @@
+/*
+Crea una variable llamada persona de tipo objeto literal con las siguientes propiedades:
+
+peso - 65
+estatura - 1.8
+bmi - una función que retorne el índice de masa corporal de la persona. Recuerda que la fórmula es peso/estatura^2 (peso sobre estatura al cuadrado).
+// escribe tu código acá
+
+// código de prueba
+console.log(persona.bmi()); // 20.061728395061728
+ */
+
+const persona = {
+  peso: 65,
+  estatura: 1.8,
+  bmi: function () {
+    return this.peso / this.estatura ** 2;
+  },
+};
+
+console.log(persona.bmi());
